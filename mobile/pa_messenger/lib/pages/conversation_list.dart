47295972cba77fb8cab580e43389f4b3ce447edf
Boris Zivkovic <boriszivk@gmail.com>
2021-01-16 00:30:28 +0100

    _calculateTimestamp implementatino

--- mobile/pa_messenger/lib/pages/conversation_list.dart
+++ mobile/pa_messenger/lib/pages/conversation_list.dart
@@ -124,14 +124,31 @@ class _ConversationListItem extends StatelessWidget {
     );
   }
 
-  _calculateTimestamp(DateTime date) {
-    return '${_stringifyNumber(date.hour)}:${_stringifyNumber(date.minute)}';
-  }
+  _calculateTimestamp(DateTime dateTime) {
+    var timestamp = 'Now';
 
-  _stringifyNumber(int number) {
-    if (number < 10) {
-      return '0' + number.toString();
+    final diff = DateTime.now().difference(dateTime);
+
+    if (diff.inDays >= 7) {
+      timestamp = '${dateTime.day}/${dateTime.month}';
     }
-    return number.toString();
+
+    if (dateTime.year != DateTime.now().year) {
+      timestamp = '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+    }
+
+    if (diff.inDays == 1) {
+      timestamp = 'Yesterday';
+    } else if (diff.inDays > 1) {
+      timestamp = '${diff.inDays} days ago';
+    } else if (diff.inHours >= 1) {
+      timestamp = '${diff.inHours}h';
+    } else if (diff.inMinutes >= 1) {
+      timestamp = '${diff.inMinutes}m';
+    } else if (diff.inSeconds > 3) {
+      timestamp = '${diff.inSeconds}s';
+    }
+
+    return timestamp;
   }
 }
