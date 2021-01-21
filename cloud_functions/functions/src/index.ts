36f30f62e69df64d820598375a2f2d328ea7edd0
Boris Zivkovic <boriszivk@gmail.com>
2021-01-22 00:34:24 +0100

    on user updated added to index

--- cloud_functions/functions/src/index.ts
+++ cloud_functions/functions/src/index.ts
@@ -1,10 +1,11 @@
 import { deleteAllAuthUsers, seedData } from './functions/seed.functions';
 import { onMessageCreated } from './functions/message.functions';
-import { onUserUpdated } from './functions/user.functions';
+import { onUserCreated, onUserUpdated } from './functions/user.functions';
 
 export {
   seedData,
   deleteAllAuthUsers,
   onMessageCreated,
+  onUserCreated,
   onUserUpdated,
 };
